16ee355520b8a2d8c44d7fd4c5f6c93dfcd77a06
ajc32199 <adam.chiarella@gmail.com>
2025-04-01 11:55:00 -0500

    basic backend functioning, GET and POST, tested with postman

--- index.js
+++ index.js
@@ -1,14 +1,53 @@
 const express = require('express');
-const cors = require('cors');
+
 const app = express();
+const port = 3000;
 
-app.use(cors());
+//middleware to parse JSON requests
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Backend is running');
+let reports = []; //temp storage
+
+app.post('/reports', (req, res) => {
+    const { latitude, longitude, description } = req.body;
+
+    if (!latitude || !longitude || !description) {
+        console.log('All fields are required');
+        //print what field was missing
+        if (!latitude) {
+            console.log('Missing latitude');
+        }
+        if (!longitude) {
+            console.log('Missing longitude');
+        }
+        if (!description) {
+            console.log('Missing description');
+        }
+        return res.status(400).json({ error: 'All fields are required' });
+        
+    }
+
+    const newReport = {
+        id: reports.length + 1,
+        latitude,
+        longitude,
+        description,
+        timestamp: new Date(),
+    };
+
+    reports.push(newReport);
+    res.status(201).json(newReport);
+    console.log('New report added:', newReport);
 });
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.get('/reports', (req, res) => {
+    res.json(reports);
+    console.log('All report(s) sent');
+    console.log('Number of reports sent: ', reports.length);
+
+});
+
+//Start server
+app.listen(port, () => {
+    console.log(`Server is running at port ${port}`);
 });
