8f82a5fd6665c8b842b8551758e43810d45db527
ajc32199 <adam.chiarella@gmail.com>
2025-04-09 08:39:45 -0500

    updated env variable in .js and dockerfile

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ admin.initializeApp({
 const db = admin.firestore();
 const app = express();
 app.use(cors());
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 //middleware to parse JSON requests
 app.use(express.json());
